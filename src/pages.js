8489a8b7a90dd9c87ec8ce727aa4b9e0c7d2423b
Nathan <NathHorrigan@gmail.com>
2019-09-10 21:38:57 +0100

    Switch up async use

--- src/pages.js
+++ src/pages.js
@@ -14,40 +14,43 @@ export const createWagtailPages = (pageMap, args, fragmentFiles = []) => {
                 }
             }
         }
-    `).then(async res => {
+    `).then(res => {
         const pages = res.data.wagtail.pages
         if (pages) {
-            // Create pages for any page objects that match the page map.
-            await pages.map(async page => {
-                const pageCacheKey = `page-${page.id}`
-                const pageHash = await cache.get(pageCacheKey)
-                if (pageHash && pageHash == page.fileHash) {
-                    const node = getNodes().find(node => node.url == url)
-                    touchNode({ nodeId: node.id })
-                    return;
-                } else {
-                    cache.set(pageCacheKey, page.hash)
-                }
-
-                const matchingKey = Object
-                    .keys(pageMap)
-                    .find(key => key.toLowerCase() == page.contentType.toLowerCase())
-                
-                if (matchingKey) {
-                    const template = pageMap[matchingKey]
-                    createPage({
-                        path: page.url,
-                        component: path.resolve('./src/' + template),
-                        context: page,
-                    })                    
-                }
-            })
-
+            
             // Create preview page and pass page-map.
             createPage({
                 path: '/preview',
                 component: path.resolve('./node_modules/gatsby-source-wagtail/preview-template.js'),
                 context: { pageMap, fragmentFiles },
+            })
+
+            // Create pages for any page objects that match the page map.
+            return pages.map(page => {
+                const pageCacheKey = `page-${page.id}`
+                cache.get(pageCacheKey).then(pageHash => {
+                    
+                    if (pageHash && pageHash == page.fileHash) {
+                        const node = getNodes().find(node => node.url == url)
+                        touchNode({ nodeId: node.id })
+                        return;
+                    } else {
+                        cache.set(pageCacheKey, page.hash)
+                    }
+    
+                    const matchingKey = Object
+                        .keys(pageMap)
+                        .find(key => key.toLowerCase() == page.contentType.toLowerCase())
+                    
+                    if (matchingKey) {
+                        const template = pageMap[matchingKey]
+                        createPage({
+                            path: page.url,
+                            component: path.resolve('./src/' + template),
+                            context: page,
+                        })                    
+                    }
+                })
             })
         }
     })
