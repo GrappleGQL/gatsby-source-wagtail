48e06707ae5bb0fbc188af37147a92607f39be1d
Fabien Le Frapper <contact@fabienlefrapper.me>
2020-11-02 18:34:43 +0100

    Take 27459cbdabec1644a199bc84b4422eb4757e3477 from gatsby-source-graphql-universal

--- src/babel-loader.js
+++ src/babel-loader.js
@@ -1,10 +1,10 @@
 const babelLoader = require(`babel-loader`)
 
 const {
-    getCustomOptions,
-    mergeConfigItemOptions,
+  getCustomOptions,
+  mergeConfigItemOptions,
 } = require(`gatsby/dist/utils/babel-loader-helpers`)
-const { prepareOptions } = require(`./utils`)
+const { prepareOptions } = require(`./utils`);
 
 /**
  * Gatsby's custom loader for webpack & babel
@@ -22,72 +22,73 @@ const { prepareOptions } = require(`./utils`)
  * You can find documentation for the custom loader here: https://babeljs.io/docs/en/next/babel-core.html#loadpartialconfig
  */
 module.exports = babelLoader.custom((babel) => {
-    const toReturn = {
-        // Passed the loader options.
-        customOptions({ stage = `test`, ...options }) {
-            return {
-                custom: {
-                    stage,
-                },
-                loader: {
-                    cacheDirectory: true,
-                    sourceType: `unambiguous`,
-                    ...getCustomOptions(stage),
-                    ...options,
-                },
-            }
+  const toReturn = {
+    // Passed the loader options.
+    customOptions({ stage = `test`, reactRuntime = `classic`, ...options }) {
+      return {
+        custom: {
+          stage,
+          reactRuntime
         },
-
-        // Passed Babel's 'PartialConfig' object.
-        config(partialConfig, { customOptions }) {
-            let { options } = partialConfig
-            const [
-                reduxPresets,
-                reduxPlugins,
-                requiredPresets,
-                requiredPlugins,
-                fallbackPresets,
-            ] = prepareOptions(babel, customOptions)
-
-            // If there is no filesystem babel config present, add our fallback
-            // presets/plugins.
-            if (!partialConfig.hasFilesystemConfig()) {
-                options = {
-                    ...options,
-                    plugins: requiredPlugins,
-                    presets: [...fallbackPresets, ...requiredPresets],
-                }
-            } else {
-                // With a babelrc present, only add our required plugins/presets
-                options = {
-                    ...options,
-                    plugins: [...options.plugins, ...requiredPlugins],
-                    presets: [...options.presets, ...requiredPresets],
-                }
-            }
-
-            // Merge in presets/plugins added from gatsby plugins.
-            reduxPresets.forEach((preset) => {
-                options.presets = mergeConfigItemOptions({
-                    items: options.presets,
-                    itemToMerge: preset,
-                    type: `preset`,
-                    babel,
-                })
-            })
-
-            reduxPlugins.forEach((plugin) => {
-                options.plugins = mergeConfigItemOptions({
-                    items: options.plugins,
-                    itemToMerge: plugin,
-                    type: `plugin`,
-                    babel,
-                })
-            })
-
-            return options
+        loader: {
+          cacheDirectory: true,
+          sourceType: `unambiguous`,
+          ...getCustomOptions(stage),
         },
-    }
+        ...options
+      }
+    },
 
-    return toReturn
+    // Passed Babel's 'PartialConfig' object.
+    config(partialConfig, { customOptions }) {
+      let { options } = partialConfig
+      const [
+        reduxPresets,
+        reduxPlugins,
+        requiredPresets,
+        requiredPlugins,
+        fallbackPresets,
+      ] = prepareOptions(babel, customOptions)
+
+      // If there is no filesystem babel config present, add our fallback
+      // presets/plugins.
+      if (!partialConfig.hasFilesystemConfig()) {
+        options = {
+          ...options,
+          plugins: requiredPlugins,
+          presets: [...fallbackPresets, ...requiredPresets],
+        }
+      } else {
+        // With a babelrc present, only add our required plugins/presets
+        options = {
+          ...options,
+          plugins: [...options.plugins, ...requiredPlugins],
+          presets: [...options.presets, ...requiredPresets],
+        }
+      }
+
+      // Merge in presets/plugins added from gatsby plugins.
+      reduxPresets.forEach(preset => {
+        options.presets = mergeConfigItemOptions({
+          items: options.presets,
+          itemToMerge: preset,
+          type: `preset`,
+          babel,
+        })
+      })
+
+      reduxPlugins.forEach(plugin => {
+        options.plugins = mergeConfigItemOptions({
+          items: options.plugins,
+          itemToMerge: plugin,
+          type: `plugin`,
+          babel,
+        })
+      })
+
+      return options
+    },
+  }
+
+  return toReturn
 })
